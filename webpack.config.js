96ea90850c321e7d6d9f9ae4396337d6ce8cd2fa
Iswenzz <suxlolz1528@gmail.com>
2020-08-24 22:07:09 +0200

    [Webpack] Added fonts file loader rule

--- webpack.config.js
+++ webpack.config.js
@@ -52,6 +52,10 @@ module.exports = {
 				test: /\.(png|jpe?g|gif|svg|webp)$/i,
 				loader: "file-loader"
 			},
+			{	// font loader
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: "file-loader"
+			},
 			{	// html loader
 				test: /\.html$/i,
 				loader: "html-loader",
